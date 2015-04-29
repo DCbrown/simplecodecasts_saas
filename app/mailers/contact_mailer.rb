b050d02ad3ce5a5a1dbe3a21662cd653e1cb21d3
Donovan Brown <Dess5000@gmail.com>
2015-04-29 04:07:00 +0000

    fixed typo found in contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'Dess5000@gmail.com'
   
-  def conact_email(name, email, body)
+  def contact_email(name, email, body)
     @name = name
-    @email = eamil
+    @email = email
     @body = body
     
     mail(from: email, subject: "Contact From Message")
