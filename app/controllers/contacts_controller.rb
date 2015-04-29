cbc9947353fd2eb47ab4455f89f34e42f7cef741
Donovan Brown <Dess5000@gmail.com>
2015-04-29 00:58:36 +0000

    Testing for contacts contoller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,21 +4,22 @@ class ContactsController < ApplicationController
   end
   
   def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      name = params [:contact][:name]
-      email = params [:contact][:email]
-      body = params [:contact][:comments]
-      
-      ContactMailer.contact_email(name, email, body).deliver
-      
-      flash[:success] = "Message sent."
-      redirect_to new_contact_path
-    else
-      flash[:danger] = "Error occured, message has not been sent."
-      redirect_to new_contact_path     
-    end
+  @contact = Contact.new(contact_params)
+  
+  if @contact.save
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    body = params[:contact][:comments]
+    
+    ContactMailer.contact_email(name, email, body).deliver
+    
+    flash[:success] = 'Message sent.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path
   end
+end
   
   private
   def contact_params
