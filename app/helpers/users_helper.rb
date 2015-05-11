36f5666c5388e43e2f9b8569bb61764b30baae62
Donovan Brown <Dess5000@gmail.com>
2015-05-11 18:28:51 +0000

    Made corrections to index, show page and correct dollar icon typo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
     elsif @user.profile.job_title == "Entrepreneur"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
-      "<i class='fa fa-dallor'></i>".html_safe
+      "<i class='fa fa-dollar'></i>".html_safe
     end
   end
 end
